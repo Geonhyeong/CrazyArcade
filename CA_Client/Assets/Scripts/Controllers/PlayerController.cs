46e5f678eb03ddf1dd2ce885533e48c36b77e560
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2022-12-28 17:23:10 +0900

    player controller 및 mapmanager 작성

--- CA_Client/Assets/Scripts/Controllers/PlayerController.cs
+++ CA_Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,37 +1,143 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Define;
 
 public class PlayerController : MonoBehaviour
 {
     public float _speed = 5.0f;
 
+    bool _isMoving = false;
+    Vector3Int _cellPos = new Vector3Int(0, 0, 0);
+    Animator _animator;
+    
+    MoveDir _dir = MoveDir.Down;
+    public MoveDir Dir
+    {
+        get { return _dir; }
+        set
+        {
+            if (_dir == value)
+                return;
+
+            switch (value)
+            {
+                case MoveDir.Up:
+                    _animator.Play("WALK_UP");
+                    break;
+                case MoveDir.Down:
+                    _animator.Play("WALK_DOWN");
+                    break;
+                case MoveDir.Left:
+                    _animator.Play("WALK_LEFT");
+                    break;
+                case MoveDir.Right:
+                    _animator.Play("WALK_RIGHT");
+                    break;
+                case MoveDir.None:
+                    if (_dir == MoveDir.Up)
+                        _animator.Play("IDLE_UP");
+                    else if (_dir == MoveDir.Down)
+                        _animator.Play("IDLE_DOWN");
+                    else if (_dir == MoveDir.Left)
+                        _animator.Play("IDLE_LEFT");
+                    else if (_dir == MoveDir.Right)
+                        _animator.Play("IDLE_RIGHT");
+                    break;
+            }
+
+            _dir = value;
+        }
+    }
+
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        Vector3 pos = Managers.Map.CurrentGrid.CellToWorld(_cellPos) + new Vector3(0.26f, 0.26f);
+        transform.position = pos;
     }
 
     void Update()
     {
+        GetDirInput();
+        UpdatePosition();
+        UpdateIsMoving();
+    }
+
+    void GetDirInput()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.up * Time.deltaTime * _speed;
+            Dir = MoveDir.Up;
         }
-        else if(Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.down * Time.deltaTime * _speed;
+            Dir = MoveDir.Down;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * Time.deltaTime * _speed;
+            Dir = MoveDir.Left;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * Time.deltaTime * _speed;
+            Dir = MoveDir.Right;
         }
         else
         {
-
+            Dir = MoveDir.None;
         }
     }
+
+    void UpdatePosition()
+    {
+        if (_isMoving == false)
+            return;
+
+        Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(_cellPos) + new Vector3(0.26f, 0.26f);
+        Vector3 moveDir = destPos - transform.position;
+
+        // 도착 여부 체크
+        float dist = moveDir.magnitude;
+        if (dist < _speed * Time.deltaTime)
+        {
+            transform.position = destPos;
+            _isMoving = false;
+        }
+        else
+        {
+            transform.position += moveDir.normalized * _speed * Time.deltaTime;
+            _isMoving = true;
+        }
+    }
+
+    void UpdateIsMoving()
+    {
+        if (_isMoving == false && _dir != MoveDir.None)
+        {
+            Vector3Int destPos = _cellPos;
+            switch (_dir)
+            {
+                case MoveDir.Up:
+                    destPos += Vector3Int.up;
+                    break;
+                case MoveDir.Down:
+                    destPos += Vector3Int.down;
+                    break;
+                case MoveDir.Left:
+                    destPos += Vector3Int.left;
+                    break;
+                case MoveDir.Right:
+                    destPos += Vector3Int.right;
+                    break;
+            }
+
+            if (Managers.Map.CanGo(destPos))
+            {
+                _cellPos = destPos;
+                _isMoving = true;
+            }
+        }
+    }
+
+    
 }
